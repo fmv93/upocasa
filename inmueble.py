a777b51257e06c8b08934201e6521898abd222f8
fmv93 <35026117+fmv93@users.noreply.github.com>
2019-01-03 10:11:01 +0000

    Update inmueble.py
    añadida relacion many2many con caracteristica 
    añadida relacio many2one con propietario

--- inmueble.py
+++ inmueble.py
@@ -55,6 +55,8 @@ class inmueble(osv.Model):
         'visita_ids':fields.one2many('visita', 'inmueble_id', 'Visitas concertadas en el inmueble'),
         'contrato_id':fields.many2one('contrato', 'Contrato'),
         'state': fields.selection([('aceptado', 'Aceptado'),('disponible', 'Disponible'), ('alquiladovendido', 'Alquilado o vendido')], 'Estados'),
+        'caracteristica_ids':fields.many2many('caracteristica','inmueble_caracteristica_rel','inmueble_id_inmueble','caracteristica_name','Caracteristicas'),
+        'propietario_id':fields.many2one('propietario','Propietario'),
     }
     
     _defaults = {'state':'aceptado'}
